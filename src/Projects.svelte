d286fc06203fd33daccd071266c18478f2e42dc4
Micheal Parks <michealparks1989@gmail.com>
2020-02-09 00:53:46 -0500

    Update Projects.svelte

--- src/Projects.svelte
+++ src/Projects.svelte
@@ -1,6 +1,12 @@
 
+<style>
+  h2 {
+    
+  }
+</style>
+
 <main>
-  <h2>Current projects</h2>
+  <h2 class="font-effect-anaglyph">Current projects</h2>
   <article>
     <h3>Galeri</h3>
   </article>
