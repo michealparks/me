e0b220fc307902dd06b2e7a5b51dfc26a7013017
Micheal Parks <michealparks1989@gmail.com>
2023-06-17 22:52:26 -0400

    remove unused import

--- src/components/synth.svelte
+++ src/components/synth.svelte
@@ -4,7 +4,6 @@ import { T } from '@threlte/core'
 import { useGltf } from '@threlte/extras'
 import { RigidBody, AutoColliders } from '@threlte/rapier'
 import { randomTransform, randomVelocities } from '../util'
-    import { shadows } from 'trzy';
 
 interface GLTF {
   nodes: {
